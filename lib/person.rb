599e5c05d774765b54f06055c23424773dadad44
ChayaGreisman <chayamgreisman@gmail.com>
2020-05-19 10:30:46 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
   :weight, :handed, :complexion, :t_shirt_size, 
-  :wrist_size, :glove_size, :pant_length, :pant_width  
+  :wrist_size, :glove_size, :pant_length, :pant_width 
+
   def initialize(attributes=nil)
-    if attributes
-      attributes.each do |key,value|
-        self.send("#{key}=", value)
-      end
+    attributes.each do |key,value|
+      self.send("#{key}=", value)
     end
   end
+
 end
 
 
